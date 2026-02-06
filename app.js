d7e271c4078a90d11633497529c535db83692294
Callum Wilcox <thebug950@gmail.com>
2026-02-06 14:30:32 -0700

    Added averages functionality

--- app.js
+++ app.js
@@ -43,6 +43,31 @@ app.get('/api/artists/:id', async (req, res) => {
     res.json(row);
 });
 
+// Getting /api/artists/averages/:id
+app.get('/api/artists/averages/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const row = await db.get(`
+    SELECT 
+      AVG(bpm) AS bpm,
+      AVG(energy) AS energy,
+      AVG(danceability) AS danceability,
+      AVG(loudness) AS loudness,
+      AVG(liveness) AS liveness,
+      AVG(valence) AS valence,
+      AVG(duration) AS duration,
+      AVG(acousticness) AS acousticness,
+      AVG(speechiness) AS speechiness,
+      AVG(popularity) AS popularity
+    FROM songs
+    WHERE artist_id = ?
+  `, id);
+
+  if (!row || Object.values(row).every(v => v === null))
+    return res.json({ error: 'Requested resource did not return any data.' });
+
+  res.json(row);
+});
 
 
 app.listen(PORT, () => {
